4caae2280e3bc684cd91d5fa51f8efe5356010fc
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-03-13 15:05:53 -0400

    updated tailwind styles to directly get from comps.css for conformity

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,25 +12,25 @@ import './comps.css';
 
 const sideBar = () => {
     return (
-        <div className='links-nav'>
+        <div className='sidebar-nav'>
                 <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[2px] duration-300 bg-blue-600 rounded-2xl'>
-                        <a className='flex pl-3 justify-between items-center w-full text-white' href='https://www.linkedin.com/in/adityapatel953/' target="_blank" rel="noopener noreferrer">
+                    <li className='sidebar-floating-box bg-blue-600'>
+                        <a className='sidebar-text' href='https://www.linkedin.com/in/adityapatel953/' target="_blank" rel="noopener noreferrer">
                             Linkedin <FaLinkedin size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[2px] duration-300 bg-[#333333] rounded-2xl'>
-                        <a className='flex pl-5 justify-between items-center w-full text-white' href='https://github.com/aspatel262' target="_blank" rel="noopener noreferrer">
+                    <li className='sidebar-floating-box bg-[#333333]'>
+                        <a className='sidebar-text' href='https://github.com/aspatel262' target="_blank" rel="noopener noreferrer">
                             Github <FaGithub size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[2px] duration-300 bg-[#6fc2b0] rounded-2xl'>
-                        <a className='flex pl-6 justify-between items-center w-full text-white' href='mailto:aspatel262@gmail.com'>
+                    <li className='sidebar-floating-box bg-[#6fc2b0]'>
+                        <a className='sidebar-text' href='mailto:aspatel262@gmail.com'>
                             Email <HiOutlineMail size={30} />
                         </a>
                     </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[2px] duration-300 bg-[#565f69] rounded-2xl'>
-                        <a className='flex pl-4 justify-between items-center w-full text-white' href={ res } target="_blank">
+                    <li className='sidebar-floating-box bg-[#565f69]'>
+                        <a className='sidebar-text' href={ res } target="_blank">
                             Resume <BsFillPersonLinesFill size={30} />
                         </a>
                     </li>

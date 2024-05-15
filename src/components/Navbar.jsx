53f1e6923cd3c155b23868373f9724bc852f9492
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-05-15 19:20:35 -0400

    new refreshed mobile menu view, including new hamburger icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import {
-  FaBars,
-  FaTimes,
-} from 'react-icons/fa';
-import {
-  FaGithub,
-  FaLinkedin
-} from 'react-icons/fa';
+import React, { useState, useEffect, useRef } from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Sling as Hamburger } from 'hamburger-react'
 import res from '../assets/Aditya_Patel_Resume.pdf';
 import navbarLogo from '../assets/logoFlip.png';
-import './comps.css';
 import { Button, Navbar } from "flowbite-react";
 import "../App.css";
+import './comps.css';
 
 function NavbarMain() {
   const [isScrolled, setIsScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);  // Create a ref for the menu
+  const [menuHeight, setMenuHeight] = useState('0px');
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    }
+  
+    if (menuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuOpen]);
+
+
+  const scrollToContact = () => {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth', block: 'start'});
+    }
+  };
+
+  
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+
+  useEffect(() => {
+    const menuNode = document.getElementById('mobile-menu');
+    if (menuOpen && menuNode) {
+      setMenuHeight(`${menuNode.scrollHeight}px`);
+    } else {
+      setMenuHeight('0px');
+    }
+  }, [menuOpen]);
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -60,14 +96,6 @@ function NavbarMain() {
     left: 0
   };
 
-  const scrollToContact = () => {
-    const contactSection = document.getElementById('contact');
-    if (contactSection) {
-      contactSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-  
-  const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <Navbar style={navbarStyle} className="fixed top-0 z-50 w-full md:w-auto">
@@ -95,43 +123,48 @@ function NavbarMain() {
 
         {/* Hamburger Menu Icon */}
         <div className="md:hidden flex items-center pr-4">
-          <button onClick={toggleMenu} className="text-white">
-            {menuOpen ? <FaTimes size={28} /> : <FaBars size={28} />}
-          </button>
+          <Hamburger color="#FFFFFF" duration={0.8} toggled={menuOpen} toggle={toggleMenu} />
         </div>
       </div>
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className="absolute top-[70px] left-0 w-full flex justify-center md:hidden">
-          <div className="bg-[rgba(0,34,48,0.85)] rounded-lg p-4 w-[90%] max-w-md shadow-lg">
+        <div id="mobile-menu" ref={menuRef} className={`absolute top-[70px] left-0 w-full flex justify-center lg:hidden transition-all duration-300 ${menuOpen ? 'mobile-menu open' : 'mobile-menu'}`} style={{ height: menuHeight }}>
+          <div style={navbarBlur} className="bg-[rgba(0,34,48,0.85)] rounded-lg p-4 w-[90%] max-w-md shadow-lg">
             <ul className="list-none p-0 m-0 space-y-2 px-4">
-              {['Home', 'About Me', 'Projects', 'Experiences'].map((item, index) => (
-                <li key={index} className="text-white text-xl py-2 hover:bg-[#3b4e68] rounded-lg transition duration-200">
-                  <a href={`/${item.toLowerCase().replace(/\s+/g, '')}`} onClick={toggleMenu}>{item}</a>
-                </li>
-              ))}
+              <li className="text-white text-xl py-2 text-center hover:bg-[#4d6b94] rounded-lg transition duration-200">
+                  <a href="/" onClick={toggleMenu}>Home</a>
+              </li>
+              <li className="text-white text-xl py-2 text-center hover:bg-[#4d6b94] rounded-lg transition duration-200">
+                  <a href="/about-me" onClick={toggleMenu}>About Me</a>
+              </li>
+              <li className="text-white text-xl py-2 text-center hover:bg-[#4d6b94] rounded-lg transition duration-200">
+                  <a href="/projects" onClick={toggleMenu}>Projects</a>
+              </li>
+              <li className="text-white text-xl py-2 text-center hover:bg-[#4d6b94] rounded-lg transition duration-200">
+                  <a href="/experiences" onClick={toggleMenu}>Experiences</a>
+              </li>
             </ul>
             <div className="mt-4 px-4">
-              <Button className="contact-btn w-full" href="/contact" onClick={toggleMenu}>Contact Me</Button>
+              <Button className="contact-btn w-full" onClick={() => {toggleMenu(); scrollToContact();}}>Contact Me</Button>
             </div>
           {/* Social Icons as Squares */}
-          <div className="mt-4 grid grid-cols-2 gap-2">
+          <div className="mt-4 grid grid-cols-2 gap-2 pb-4">
               <a className='relative bg-gradient-to-r from-[#6a11cb] to-[#2575fc] hover:from-blue-600 hover:to-blue-800 hover:scale-105 text-white flex items-center justify-center h-12 rounded-md overflow-hidden group transition duration-200 ease-in-out' href='https://www.linkedin.com/in/adityapatel953/' target="_blank" rel="noopener noreferrer">
                 <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200'>LinkedIn</span>
                 <FaLinkedin size={28} className='group-hover:opacity-0 transition-opacity duration-200' />
               </a>
-              <a className='relative bg-gradient-to-r from-[#555555] to-[#333333] hover:from-gray-700 hover:to-gray-600 hover:scale-105 text-white flex items-center justify-center h-12 rounded-md overflow-hidden group transition duration-200 ease-in-out' href='https://github.com/aspatel262' target="_blank" rel="noopener noreferrer">
+              <a className='relative bg-gradient-to-r from-gray-700 to-gray-600 hover:from-[#555555] hover:to-[#333333] hover:scale-105 text-white flex items-center justify-center h-12 rounded-md overflow-hidden group transition duration-200 ease-in-out' href='https://github.com/aspatel262' target="_blank" rel="noopener noreferrer">
                 <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200'>Github</span>
                 <FaGithub size={28} className='group-hover:opacity-0 transition-opacity duration-200' />
-              </a>
-              <a className='relative bg-gradient-to-r from-[#6fc2b0] to-[#28a79f] hover:from-[#28a79f] hover:to-[#6fc2b0] hover:scale-105 text-white flex items-center justify-center h-12 rounded-md overflow-hidden group transition duration-200 ease-in-out' href='mailto:aspatel262@gmail.com'>
-                <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200'>Send An Email</span>
-                <HiOutlineMail size={28} className='group-hover:opacity-0 transition-opacity duration-200' />
               </a>
               <a className='relative bg-gradient-to-r from-[#444444] to-[#999999] hover:from-[#999999] hover:to-[#444444] hover:scale-105 text-white flex items-center justify-center h-12 rounded-md overflow-hidden group transition duration-200 ease-in-out' href={res} target="_blank" rel="noopener noreferrer">
                 <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200'>Download Resume</span>
                 <BsFillPersonLinesFill size={28} className='group-hover:opacity-0 transition-opacity duration-200' />
+              </a>
+              <a className='relative bg-gradient-to-r from-[#6fc2b0] to-[#28a79f] hover:from-[#28a79f] hover:to-[#6fc2b0] hover:scale-105 text-white flex items-center justify-center h-12 rounded-md overflow-hidden group transition duration-200 ease-in-out' href='mailto:aspatel262@gmail.com'>
+                <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200'>Send An Email</span>
+                <HiOutlineMail size={28} className='group-hover:opacity-0 transition-opacity duration-200' />
               </a>
             </div>
           </div>

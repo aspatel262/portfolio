b5fb18b70146af7190524a8813a6f106776142b1
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-05-23 02:03:23 -0400

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 import React from "react";
 import Home from './components/Home.jsx';
 import AboutMe from './components/AboutMe.jsx';

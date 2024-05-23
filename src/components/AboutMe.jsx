524b21f019b1fb65841ad89cee9c5e222bbda090
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-05-23 01:08:04 -0400

    migrated all styles from tailwind inline to style sheet as generally accepted

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,31 +4,28 @@ import headshot from '../assets/aboutme/headshot.png'
 
 const AboutMe = () => {
   return (
-    <div id="about-me" className="w-full min-h-screen bg-white flex flex-col items-center">
-      <div className="about-me-container pt-20 flex flex-col md:flex-row items-center justify-between w-full min-h-screen md:pl-44 md:pr-20 p-8 overflow-auto">
-        <div className="image-container flex items-center justify-center flex-shrink-0 mb-6 md:mb-0 md:mr-6 w-full md:w-2/5 md:-rotate-90 md:hover:-rotate-0 md:transition-all md:ease-in-out md:duration-300">
-          <img 
-            src={headshot}
-            alt="Aditya Patel" 
-            className="flex-shrink-0 mb-6 md:mb-0 md:w-full w-3/4 h-auto object-cover"
-          />
+    <div id="about-me" className="abme">
+      <div className="abme-page">
+        <div className="image-container">
+          <img src={headshot} alt="Headshot of Aditya Patel" />
         </div>
         
-        <div className="text-container w-full md:w-3/5 md:pl-5 md:mt-20 lg:mt-0">
-          <h1 className="text-2xl md:text-3xl lg:text-3xl font-bold mb-4">
+        <div className="about-content">
+          <h1>
             I'm Aditya Patel, a Software Engineer
           </h1>
-          <p className="text-lg lg:text-xl text-gray-700 mb-4">
+          <p className="about-text">
             I’ve spent the past few years studying the latest and greatest technologies to pursue my passion in the field of Computer Science. Currently, 
-            I am a student at the University of Michigan Ann Arbor, but my interest in what I learn expands far beyond campus. 
+            I am a <span className='about-bolded'>student at the University of Michigan Ann Arbor</span>, but my interest in what I learn expands far beyond campus. 
           </p>
-          <p className="text-lg lg:text-xl text-gray-700 mb-4">
-            Driven by an insatiable curiosity and a constant thirst for learning, I continually seek to explore new areas and deepen my understanding 
-            of the tech world. This curiosity has led me to embrace a variety of professional experiences, working across different areas of software 
-            engineering, machine learning, data science engineering, front-end and back-end systems development. My primary goal is to create impactful,
-            applicable user oriented solutions to problems that make a difference.
+          <p className="about-text">
+            Driven by an <span className='about-bolded'>insatiable curiosity</span> and a <span className='about-bolded'>constant thirst for learning</span>, 
+            I continually seek to explore new areas and deepen my understanding of the tech world. This curiosity has led me to embrace a variety of 
+            professional experiences, working across different areas of software engineering, machine learning, data science engineering, front-end 
+            and back-end systems development. <span className='about-bolded'>My primary goal</span> is to create impactful, applicable user 
+            oriented solutions to problems that <span className='about-bolded'>make a difference</span>.
           </p>
-          <p className="text-lg lg:text-xl text-gray-700 mb-4">
+          <p className="about-text">
             Outside of my IDE, you will find me pursuing my hobbies. I enjoy playing the electric guitar, viewing the world 
             behind the lens of my camera, as well as following and playing sports such as basketball, tennis, and football. 
             Come see more about my life below!

bd9f6b12c8918ec82e6cb2d1749ad6cc1e49e00e
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-05-23 02:15:08 -0400

    did allat work

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
 
     <footer className="footer">
-      <div className="footer-container">
+      <div className="footer-container container">
         <p>
           &copy; {new Date().getFullYear()} Aditya Patel. All rights reserved.
         </p>

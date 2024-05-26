ad2332be3dabd2c4905f6777782211a47e6bb9c4
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-05-26 16:50:27 -0400

    printing req type

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
+  console.log(`Received ${req.method} request`);
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }

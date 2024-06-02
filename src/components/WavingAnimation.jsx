518066c07bbd634e9ff409449e48fb1b859b5c8a
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-02 03:30:41 -0400

    rearranged dir structure

--- src/components/WavingAnimation.jsx
+++ src/components/WavingAnimation.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 // Import frames
-import frame1 from '../assets/waving_frames/frame_1.png';
-import frame2 from '../assets/waving_frames/frame_2.png';
-import frame3 from '../assets/waving_frames/frame_3.png';
-import frame4 from '../assets/waving_frames/frame_4.png';
-import frame5 from '../assets/waving_frames/frame_5.png';
-import frame6 from '../assets/waving_frames/frame_6.png';
-import frame7 from '../assets/waving_frames/frame_7.png';
-import frame8 from '../assets/waving_frames/frame_8.png';
+import frame1 from '../assets/waving-frames/frame_1.png';
+import frame2 from '../assets/waving-frames/frame_2.png';
+import frame3 from '../assets/waving-frames/frame_3.png';
+import frame4 from '../assets/waving-frames/frame_4.png';
+import frame5 from '../assets/waving-frames/frame_5.png';
+import frame6 from '../assets/waving-frames/frame_6.png';
+import frame7 from '../assets/waving-frames/frame_7.png';
+import frame8 from '../assets/waving-frames/frame_8.png';
 
 // Array of frames
 const frames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8];

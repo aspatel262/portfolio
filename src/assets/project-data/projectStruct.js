27554191886d3b0f04d72e3e664766e415b38d67
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-03 18:51:23 -0400

    wrote structure with current projets

--- src/assets/project-data/projectStruct.js
+++ src/assets/project-data/projectStruct.js
@@ -2,12 +2,13 @@ const projects = [
     {
         title: 'Study Buddy',
         description: 'This is a study-helper web app project made to make your life easier when catching up on lecture content in an interactive way.',
+        image: '/project-data/project-image/studybuddy.jpeg',
         details: {
             description: 'Study Buddy is a web platform designed to enhance the learning experience by providing automated summarization of educational materials and creating interactive quizzes tailored to the uploaded content. It converts text documents, PDFs, and images into concise summaries for quick review, and generates quizzes based on the uploaded content to help users assess their understanding. The platform features a user-friendly interface that is both professional and aesthetically appealing, making it an effective and engaging tool for students.',
             github: 'https://github.com/aspatel262/Study-Buddy',
             liveDemo: 'https://github.com/aspatel262/Study-Buddy',
             techStack: [
-                { name: 'React', link: 'https://reactjs.org', icon: '/project-data/icons/react.svg' },
+                { name: 'React', link: 'https://reactjs.org', icon: '/assets/project-data/icons/react.svg' },
                 { name: 'Flask', link: 'https://flask.palletsprojects.com', icon: '/project-data/icons/flask.svg' },
                 { name: 'TailwindCSS', link: 'https://tailwindcss.com', icon: '/project-data/icons/tailwindcss.svg' },
                 { name: 'Google Gemini 1.5 API', link: 'https://cloud.google.com/ai/gemini', icon: '/project-data/icons/google-gemini.svg' },
@@ -19,6 +20,7 @@ const projects = [
     {
         title: 'Chipotle Freepointer Burrito Bot',
         description: 'Automated bot to score free burrito codes during the 2023 NBA Finals',
+        image: '/src/assets/project-data/project-image/freepointer.png',
         details: {
             description: 'This bot scans Chipotle\'s Twitter account for promotional "FREE" burrito codes and automatically texts these codes to users. It aims to simplify the process of obtaining and using Chipotle promotions. Users need to provide a Twitter API key and configure email settings for the bot to function properly. The project addresses the need for automated and efficient code retrieval and distribution.',
             github: 'https://github.com/aspatel262/freechipotle',

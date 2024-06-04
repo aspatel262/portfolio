38926124bc690e5e18e92cc75970882680cef685
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-04 01:42:37 -0400

    implemented pop up menu for project view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -125,7 +125,60 @@ const Projects = () => {
                 </div>
             </div>
 
-            
+            <AnimatePresence>
+                {selectedProject && (
+                    <motion.div 
+                        layoutId={selectedProject.id} 
+                        className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50 p-10 pt-12"  // Increased padding to 10
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                        onClick={handleBackdropClick}
+                    >
+                        <motion.div 
+                            className="bg-white p-10 rounded-lg shadow-lg max-w-5xl w-full flex flex-col md:flex-row overflow-y-auto"
+                            initial="hidden"
+                            animate="visible"
+                            exit="hidden"
+                            variants={{
+                                hidden: { opacity: 0 },
+                                visible: { opacity: 1, transition: { staggerChildren: 0.2 } }
+                            }}
+                            style={{ maxHeight: '90vh' }} // Ensure the modal doesn't exceed the viewport height
+                        >
+
+                            <div className="flex-shrink-0 flex items-center justify-center">
+                                <motion.img src={projectImages[imageIdx]} alt={selectedProject.title} className="rounded-lg max-w-full md:max-w-sm m-5" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }}/>
+                            </div>
+                            <div className="flex-1 flex flex-col mb-4 md:mb-0">
+                                <motion.h2 variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }} className="text-2xl font-bold mb-4">{selectedProject.title}</motion.h2>
+                                <motion.p variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }} className="mb-4">{selectedProject.details.description}</motion.p>
+                                <motion.div variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }} className="mb-4">
+                                    <motion.h3 className="text-xl font-bold mb-2">Tech Stack</motion.h3>
+                                    <ul className="flex flex-wrap tech-stack-list">
+                                        {selectedProject.details.techStack.map((tech, index) => (
+                                            <li key={index} className="tech-stack-item">
+                                                <a href={tech.link} target="_blank" rel="noopener noreferrer" className="tech-stack-link">
+                                                    <img src={techIcons[tech.name]} alt={tech.name} className="tech-stack-icon" />
+                                                    <span className="tech-stack-tooltip">{tech.name}</span>
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </motion.div>
+                                <motion.div variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }} className="flex items-end justify-between mb-4">
+                                    <div className="flex space-x-4">
+                                    <a href={selectedProject.details.github} target="_blank" rel="noopener noreferrer" className="mr-4 px-4 py-2 bg-gray-800 text-white rounded transition-transform duration-300 hover:rotate-12 hover:bg-gray-700">GitHub Repository</a>
+                                    <a href={selectedProject.details.liveDemo} target="_blank" rel="noopener noreferrer" className="px-4 py-2 bg-blue-600 text-white rounded transition-transform duration-300 hover:rotate-12 hover:bg-blue-700">Live Demo</a>
+                                    </div>
+                                    <motion.button onClick={closeProjectDetails} className="px-4 py-2 bg-red-600 text-white rounded transition-transform duration-300 hover:rotate-12 hover:bg-red-700">Close</motion.button>
+                                </motion.div>
+                            </div>
+                        </motion.div>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };

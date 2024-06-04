2800c2f0f0b12b6b4818f595f00a363419aa18fe
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-04 01:39:18 -0400

    added projects route to router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Analytics } from "@vercel/analytics/react"
-import React from "react";
 import Home from './components/Home.jsx';
 import AboutMe from './components/AboutMe.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Projects from './components/Projects.jsx';
 
 
 export default function App() {
@@ -15,8 +16,9 @@ export default function App() {
        <Analytics/>
         <Routes>
           <Route path="/" element={<Home />} exact />
-          <Route path="/contact" element={<Home />} /> {/* Points to Home but scrolls to contact */}
+          <Route path="/contact" element={<Home />} />
           <Route path="/about-me" element={<AboutMe />} exact />
+          <Route path="/projects" element={<Projects />} exact />
         </Routes>
       <Footer />
     </Router>

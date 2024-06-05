3c85672bad408954d11a5159cd5034dddd71d859
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-05 03:43:41 -0400

    modified project struct depending on if links are available to repos

--- src/assets/project-data/projectStruct.js
+++ src/assets/project-data/projectStruct.js
@@ -5,7 +5,7 @@ const projects = [
         details: {
             description: 'Study Buddy is a web platform designed to enhance the learning experience by providing automated summarization of educational materials and creating interactive quizzes tailored to the uploaded content. It converts text documents, PDFs, and images into concise summaries for quick review, and generates quizzes based on the uploaded content to help users assess their understanding. The platform features a user-friendly interface that is both professional and aesthetically appealing, making it an effective and engaging tool for students.',
             github: 'https://github.com/aspatel262/Study-Buddy',
-            liveDemo: 'https://github.com/aspatel262/Study-Buddy',
+            liveDemo: 'no-demo',
             techStack: [
                 { name: 'Python', link: 'https://python.org' },
                 { name: 'JavaScript', link: 'https://www.javascript.com/' },
@@ -24,8 +24,8 @@ const projects = [
         description: 'My first ML project, using Naive Bayes reinforcement learning to classify the topic and author of course forum posts',
         details: {
             description: 'This project analyzes the topic of student help forum postings and classifies what the post is about using the Naive Bayes classification algorithm. The Naive Bayes algorithm uses calculating log probability scores based on word occurences in training data to make classifications. This project involves handling data preprocessing, training the model, and evaluating its performance to ensure accurate classification. Implemented in C++, the project demonstrates the application of machine learning techniques for text classification.',
-            github: 'https://github.com/aspatel262/p5-ML',
-            liveDemo: 'https://eecs280staff.github.io/p5-ml/',
+            github: 'priv-repo',
+            liveDemo: 'no-demo',
             techStack: [
                 { name: 'Naive Bayes Classification Algorithm', link: 'https://en.wikipedia.org/wiki/Naive_Bayes_classifier' },
                 { name: 'Machine Learning', link: 'https://en.wikipedia.org/wiki/Machine_learning' },
@@ -39,7 +39,7 @@ const projects = [
         details: {
             description: 'This bot scans Chipotle\'s Twitter account for promotional "FREE" burrito codes and automatically texts these codes to users. It aims to simplify the process of obtaining and using Chipotle promotions. Users need to provide a Twitter API key and configure messaging settings for the bot to function properly. The project addresses the need for automated and efficient code retrieval and distribution. The bot works by scraping twitter to find codes, filtering the tweets for the code by an algorithm, which then immediately messages the code to Chipotle within milliseconds.',
             github: 'https://github.com/aspatel262/freechipotle',
-            liveDemo: 'https://github.com/aspatel262/freechipotle',
+            liveDemo: 'no-demo',
             techStack: [
                 { name: 'Python', link: 'https://python.org' },
                 { name: 'Twitter API', link: 'https://developer.twitter.com/en/docs/twitter-api' },
@@ -55,7 +55,7 @@ const projects = [
         details: {
             description: 'This is my personal portfolio website, where I showcase my projects, skills, and experiences. It includes detailed descriptions of my work, links to GitHub repositories, and live demos. Additionally, you will learn more about the developer, me, my professional goals, and my hobbies. The website is designed to provide a comprehensive view of my capabilities and achievements as a developer, as well as a look into the things I enjoy most about my life. I built it using React, along with node.js server-less functions on Vercel to handle contact emails.',
             github: 'https://github.com/aspatel262/freechipotle',
-            liveDemo: 'https://github.com/aspatel262/freechipotle',
+            liveDemo: 'adityapatel.vercel.app',
             techStack: [
                 { name: 'JavaScript', link: 'https://www.javascript.com/' },
                 { name: 'React', link: 'https://reactjs.org' },
@@ -73,7 +73,8 @@ const projects = [
         description: 'Developed an autonomous drone piloting software capable of navigating complex environments to deliver medical supplies.',
         details: {
             description: 'This project was designed as a proof of concept for replacing delivery of medical samples, testing kits, and treatments with the use of autonomous drones in a city-like environment. It was built using PID-controller methodology based on lidar, sonar, and barometric input data for collision avoidance, as well as GPS cordinates for routing. This project was deployed and tested using Unity and Unreal Engine city virtual environments for real physics simulations to test the controller. It is entirely developed using python, including a library to control drone functionality, with Numpy and Matplotlib for data visualization.',
-            github: 'https://github.com/your-repo-link',
+            github: 'priv-repo',
+            liveDemo: 'no-demo',
             techStack: [
                 { name: 'Python', link: 'https://python.org' },
                 { name: 'Unity', link: 'https://unity.com/' },

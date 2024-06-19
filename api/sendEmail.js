d724e18d5dd42589b6edc1ae5e8eb321c200fb53
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-19 18:00:19 -0400

    hmmmmph

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -14,6 +14,7 @@ module.exports = async (req, res) => {
     const { firstName, lastName, sender, subject, message } = req.body;
 
     console.log('Request body:', req.body);
+    console.log(firstName, lastName, sender, subject, message);
 
     try {
       await sendEmail(firstName, lastName, sender, subject, message);

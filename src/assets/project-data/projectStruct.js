3a101567e8199f3c2fb64f0fedfd749ab1d8d1ed
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-06-21 02:13:06 -0400

    refactored project data rendering

--- src/assets/project-data/projectStruct.js
+++ src/assets/project-data/projectStruct.js
@@ -1,86 +1,128 @@
+// projects.js
+import imgStudyBuddy from './project-image/studybuddy.jpg';
+import imgFreePointer from './project-image/freepointer.jpeg';
+import imgPortfolio from './project-image/portfolio.png';
+import imgPostClassifier from './project-image/postclassifier.png';
+import imgDronePilot from './project-image/drone.png';
+
+import beautifulSoupIcon from './tech-stack-images/beautiful-soup.svg';
+import flaskIcon from './tech-stack-images/flask.svg';
+import googleGeminiIcon from './tech-stack-images/google-gemini.svg';
+import nodejsIcon from './tech-stack-images/nodejs.svg';
+import pythonIcon from './tech-stack-images/python.svg';
+import reactIcon from './tech-stack-images/react.svg';
+import seleniumIcon from './tech-stack-images/selenium.svg';
+import tailwindcssIcon from './tech-stack-images/tailwindcss.svg';
+import twilioIcon from './tech-stack-images/twilio.svg';
+import twitterIcon from './tech-stack-images/twitter.svg';
+import javascriptIcon from './tech-stack-images/javascript.svg';
+import gmailIcon from './tech-stack-images/gmail.svg';
+import vercelIcon from './tech-stack-images/vercel.svg';
+import htmlIcon from './tech-stack-images/html-1.svg';
+import cssIcon from './tech-stack-images/css-3.svg';
+import cppIcon from './tech-stack-images/c++.svg';
+import mlIcon from './tech-stack-images/ml.svg';
+import nbIcon from './tech-stack-images/classification.svg';
+import unityIcon from './tech-stack-images/unity.svg';
+import unrealIcon from './tech-stack-images/unreal.svg';
+import numpyIcon from './tech-stack-images/numpy.svg';
+import matplotlibIcon from './tech-stack-images/matplotlib.svg';
+import amazonAWS from './tech-stack-images/aws.svg';
+
 const projects = [
     {
         title: 'Study Buddy',
         description: 'This is a study-helper web app project made to make your life easier when catching up on lecture content in an interactive way.',
+        image: imgStudyBuddy,
+        alt: 'Study Buddy Project Image',
         details: {
             description: 'Study Buddy is a web platform designed to enhance the learning experience by providing automated summarization of educational materials and creating interactive quizzes tailored to the uploaded content. It converts text documents, PDFs, and images into concise summaries for quick review, and generates quizzes based on the uploaded content to help users assess their understanding. The platform features a user-friendly interface that is both professional and aesthetically appealing, making it an effective and engaging tool for students.',
             github: 'https://github.com/aspatel262/Study-Buddy',
             liveDemo: 'no-demo',
             techStack: [
-                { name: 'Python', link: 'https://python.org' },
-                { name: 'JavaScript', link: 'https://www.javascript.com/' },
-                { name: 'Flask', link: 'https://flask.palletsprojects.com' },
-                { name: 'React', link: 'https://reactjs.org' },
-                { name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-                { name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-                { name: 'Node.js', link: 'https://nodejs.org' },
-                { name: 'TailwindCSS', link: 'https://tailwindcss.com' },
-                { name: 'Google Gemini 1.5 API', link: 'https://cloud.google.com/ai/gemini' }
-            ]  
+                { name: 'Python', link: 'https://python.org', img: pythonIcon },
+                { name: 'JavaScript', link: 'https://www.javascript.com/', img: javascriptIcon },
+                { name: 'Flask', link: 'https://flask.palletsprojects.com', img: flaskIcon },
+                { name: 'React', link: 'https://reactjs.org', img: reactIcon },
+                { name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML', img: htmlIcon },
+                { name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS', img: cssIcon },
+                { name: 'Node.js', link: 'https://nodejs.org', img: nodejsIcon },
+                { name: 'TailwindCSS', link: 'https://tailwindcss.com', img: tailwindcssIcon },
+                { name: 'Google Gemini 1.5 API', link: 'https://cloud.google.com/ai/gemini', img: googleGeminiIcon }
+            ]
         },
     },
     {
         title: 'Course Forum Machine Learning Classifier',
         description: 'My first ML project, using Naive Bayes reinforcement learning to classify the topic and author of course forum posts',
+        image: imgPostClassifier,
+        alt: 'Post Classifier Project Image',
         details: {
-            description: 'This project analyzes the topic of student help forum postings and classifies what the post is about using the Naive Bayes classification algorithm. The Naive Bayes algorithm uses calculating log probability scores based on word occurences in training data to make classifications. This project involves handling data preprocessing, training the model, and evaluating its performance to ensure accurate classification. Implemented in C++, the project demonstrates the application of machine learning techniques for text classification.',
+            description: 'This project analyzes the topic of student help forum postings and classifies what the post is about using the Naive Bayes classification algorithm. The Naive Bayes algorithm uses calculating log probability scores based on word occurrences in training data to make classifications. This project involves handling data preprocessing, training the model, and evaluating its performance to ensure accurate classification. Implemented in C++, the project demonstrates the application of machine learning techniques for text classification.',
             github: 'priv-repo',
             liveDemo: 'no-demo',
             techStack: [
-                { name: 'Naive Bayes Classification Algorithm', link: 'https://en.wikipedia.org/wiki/Naive_Bayes_classifier' },
-                { name: 'Machine Learning', link: 'https://en.wikipedia.org/wiki/Machine_learning' },
-                { name: 'C++', link: 'https://isocpp.org' }
+                { name: 'Naive Bayes Classification Algorithm', link: 'https://en.wikipedia.org/wiki/Naive_Bayes_classifier', img: nbIcon },
+                { name: 'Machine Learning', link: 'https://en.wikipedia.org/wiki/Machine_learning', img: mlIcon },
+                { name: 'C++', link: 'https://isocpp.org', img: cppIcon }
             ]
         }
     },
     {
         title: 'Chipotle Freepointer Burrito Bot',
-        description: 'I designed an automated bot to score free burrito codes during the 2023 NBA Finals. Why, you ask? Because I love chipotle!',
+        description: 'I designed an automated bot to score free burrito codes during the 2023 NBA Finals. Why, you ask? Because I love Chipotle!',
+        image: imgFreePointer,
+        alt: 'Chipotle FreePointer Project Image',
         details: {
-            description: 'This bot scans Chipotle\'s Twitter account for promotional "FREE" burrito codes and automatically texts these codes to users. It aims to simplify the process of obtaining and using Chipotle promotions. Users need to provide a Twitter API key and configure messaging settings for the bot to function properly. The project addresses the need for automated and efficient code retrieval and distribution. The bot works by scraping twitter to find codes, filtering the tweets for the code by an algorithm, which then immediately messages the code to Chipotle within milliseconds.',
+            description: 'This bot scans Chipotle\'s Twitter account for promotional "FREE" burrito codes and automatically texts these codes to users. It aims to simplify the process of obtaining and using Chipotle promotions. Users need to provide a Twitter API key and configure messaging settings for the bot to function properly. The project addresses the need for automated and efficient code retrieval and distribution. The bot works by scraping Twitter to find codes, filtering the tweets for the code by an algorithm, which then immediately messages the code to Chipotle within milliseconds.',
             github: 'https://github.com/aspatel262/freechipotle',
             liveDemo: 'no-demo',
             techStack: [
-                { name: 'Python', link: 'https://python.org' },
-                { name: 'Twitter API', link: 'https://developer.twitter.com/en/docs/twitter-api' },
-                { name: 'Twilio API', link: 'https://www.twilio.com' },
-                { name: 'Beautiful Soup', link: 'https://www.crummy.com/software/BeautifulSoup/' },
-                { name: 'Selenium', link: 'https://www.selenium.dev' }
+                { name: 'Python', link: 'https://python.org', img: pythonIcon },
+                { name: 'Twitter API', link: 'https://developer.twitter.com/en/docs/twitter-api', img: twitterIcon },
+                { name: 'Twilio API', link: 'https://www.twilio.com', img: twilioIcon },
+                { name: 'Beautiful Soup', link: 'https://www.crummy.com/software/BeautifulSoup/', img: beautifulSoupIcon },
+                { name: 'Selenium', link: 'https://www.selenium.dev', img: seleniumIcon }
             ]
         }
     },
     {
         title: 'Aditya Patel dot Com',
         description: 'My personal portfolio website. Yup! The one you are looking at right now! Come check out what\'s under the hood!',
+        image: imgPortfolio,
+        alt: 'Personal Portfolio Project Image',
         details: {
             description: 'This is my personal portfolio website, where I showcase my projects, skills, and experiences. It includes detailed descriptions of my work, links to GitHub repositories, and live demos. Additionally, you will learn more about the developer, me, my professional goals, and my hobbies. The website is designed to provide a comprehensive view of my capabilities and achievements as a developer, as well as a look into the things I enjoy most about my life. I built it using React, along with node.js server-less functions on Vercel to handle contact emails.',
             github: 'https://github.com/aspatel262/freechipotle',
             liveDemo: 'adityapatel.vercel.app',
             techStack: [
-                { name: 'JavaScript', link: 'https://www.javascript.com/' },
-                { name: 'React', link: 'https://reactjs.org' },
-                { name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
-                { name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
-                { name: 'TailwindCSS', link: 'https://tailwindcss.com' },
-                { name: 'Node.js', link: 'https://nodejs.org' },
-                { name: 'Vercel', link: 'https://vercel.com/home' },
-                { name: 'Gmail API', link: 'https://developers.google.com/gmail/api/guides' }
+                { name: 'JavaScript', link: 'https://www.javascript.com/', img: javascriptIcon },
+                { name: 'React', link: 'https://reactjs.org', img: reactIcon },
+                { name: 'HTML', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML', img: htmlIcon },
+                { name: 'CSS', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS', img: cssIcon },
+                { name: 'TailwindCSS', link: 'https://tailwindcss.com', img: tailwindcssIcon },
+                { name: 'Node.js', link: 'https://nodejs.org', img: nodejsIcon },
+                { name: 'Vercel', link: 'https://vercel.com/home', img: vercelIcon },
+                { name: 'Gmail API', link: 'https://developers.google.com/gmail/api/guides', img: gmailIcon },
+                { name: 'Amazon AWS', link: 'https://aws.amazon.com', img: amazonAWS }
             ]
         }
     },
     {
         title: 'Autonomous Medical Delivery Drone Pilot',
         description: 'Developed an autonomous drone piloting software capable of navigating complex environments to deliver medical supplies.',
+        image: imgDronePilot,
+        alt: 'Drone Pilot Project Image',
         details: {
-            description: 'This project was designed as a proof of concept for replacing delivery of medical samples, testing kits, and treatments with the use of autonomous drones in a city-like environment. It was built using PID-controller methodology based on lidar, sonar, and barometric input data for collision avoidance, as well as GPS cordinates for routing. This project was deployed and tested using Unity and Unreal Engine city virtual environments for real physics simulations to test the controller. It is entirely developed using python, including a library to control drone functionality, with Numpy and Matplotlib for data visualization.',
+            description: 'This project was designed as a proof of concept for replacing delivery of medical samples, testing kits, and treatments with the use of autonomous drones in a city-like environment. It was built using PID-controller methodology based on lidar, sonar, and barometric input data for collision avoidance, as well as GPS coordinates for routing. This project was deployed and tested using Unity and Unreal Engine city virtual environments for real physics simulations to test the controller. It is entirely developed using Python, including a library to control drone functionality, with Numpy and Matplotlib for data visualization.',
             github: 'priv-repo',
             liveDemo: 'no-demo',
             techStack: [
-                { name: 'Python', link: 'https://python.org' },
-                { name: 'Unity', link: 'https://unity.com/' },
-                { name: 'Unreal Engine', link: 'https://www.unrealengine.com/' },
-                { name: 'Numpy', link: 'https://numpy.org/' },
-                { name: 'Matplotlib', link: 'https://matplotlib.org/' },
+                { name: 'Python', link: 'https://python.org', img: pythonIcon },
+                { name: 'Unity', link: 'https://unity.com/', img: unityIcon },
+                { name: 'Unreal Engine', link: 'https://www.unrealengine.com/', img: unrealIcon },
+                { name: 'Numpy', link: 'https://numpy.org/', img: numpyIcon },
+                { name: 'Matplotlib', link: 'https://matplotlib.org/', img: matplotlibIcon },
             ]
         }
     },
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,79 +1,15 @@
+// ProjectsList.js
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import projects from '../assets/project-data/projectStruct';
 import '../App.css';
 
-import imgStudyBuddy from '../assets/project-data/project-image/studybuddy.jpg';
-import imgFreePointer from '../assets/project-data/project-image/freepointer.jpeg';
-import imgPortfolio from '../assets/project-data/project-image/portfolio.png';
-import imgPostClassifier from '../assets/project-data/project-image/postclassifier.png';
-import imgDronePilot from '../assets/project-data/project-image/drone.png';
-
-import beautifulSoupIcon from '../assets/project-data/tech-stack-images/beautiful-soup.svg';
-import flaskIcon from '../assets/project-data/tech-stack-images/flask.svg';
-import googleGeminiIcon from '../assets/project-data/tech-stack-images/google-gemini.svg';
-import nodejsIcon from '../assets/project-data/tech-stack-images/nodejs.svg';
-import pythonIcon from '../assets/project-data/tech-stack-images/python.svg';
-import reactIcon from '../assets/project-data/tech-stack-images/react.svg';
-import seleniumIcon from '../assets/project-data/tech-stack-images/selenium.svg';
-import tailwindcssIcon from '../assets/project-data/tech-stack-images/tailwindcss.svg';
-import twilioIcon from '../assets/project-data/tech-stack-images/twilio.svg';
-import twitterIcon from '../assets/project-data/tech-stack-images/twitter.svg';
-import javascriptIcon from '../assets/project-data/tech-stack-images/javascript.svg';
-import gmailIcon from '../assets/project-data/tech-stack-images/gmail.svg';
-import vercelIcon from '../assets/project-data/tech-stack-images/vercel.svg';
-import htmlIcon from '../assets/project-data/tech-stack-images/html-1.svg';
-import cssIcon from '../assets/project-data/tech-stack-images/css-3.svg';
-import cppIcon from '../assets/project-data/tech-stack-images/c++.svg';
-import mlIcon from '../assets/project-data/tech-stack-images/ml.svg';
-import nbIcon from '../assets/project-data/tech-stack-images/classification.svg';
-import unityIcon from '../assets/project-data/tech-stack-images/unity.svg';
-import unrealIcon from '../assets/project-data/tech-stack-images/unreal.svg';
-import numpyIcon from '../assets/project-data/tech-stack-images/numpy.svg';
-import matplotlibIcon from '../assets/project-data/tech-stack-images/matplotlib.svg';
-
-
 const ProjectsList = () => {
     const [selectedProject, setSelectedProject] = useState(null);
-    const [imageIdx, setImageIdx] = useState(null);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    /*  ##### SET THE ORDER OF IMAGES HERE FOR CORRESPONDING PROJECTS #####  */ 
-    const projectImages = [imgStudyBuddy, imgPostClassifier, imgFreePointer, imgPortfolio, imgDronePilot ];
-
-    /* ##### BOOL ARRAY IF GITHUB LINK IS AVAILABLE AND DEMO IS AVAILABLE ##### */
-    const gitPrivate = [false, true, false, false, true];
-    const demoSoon = [true, true, true, true, true];
-    
-    /*  ##### ADD ALL TECH STACK ICON OBJECTS HERE #####  */
-    const techIcons = {
-        'Beautiful Soup': beautifulSoupIcon,
-        'Flask': flaskIcon,
-        'Google Gemini 1.5 API': googleGeminiIcon,
-        'Node.js': nodejsIcon,
-        'Python': pythonIcon,
-        'React': reactIcon,
-        'Selenium': seleniumIcon,
-        'TailwindCSS': tailwindcssIcon,
-        'Twilio API': twilioIcon,
-        'Twitter API': twitterIcon,
-        'JavaScript': javascriptIcon,
-        'HTML': htmlIcon,
-        'CSS': cssIcon,
-        'Gmail API': gmailIcon,
-        'Vercel': vercelIcon,
-        'Naive Bayes Classification Algorithm': nbIcon,
-        'Machine Learning': mlIcon,
-        'C++': cppIcon,
-        'Unity': unityIcon,
-        'Unreal Engine': unrealIcon,
-        'Matplotlib': matplotlibIcon,
-        'Numpy': numpyIcon,
-    };
-
-    const openProjectDetails = (project, imgIndex) => {
+    const openProjectDetails = (project) => {
         setSelectedProject(project);
-        setImageIdx(imgIndex);
     };
 
     const closeProjectDetails = () => {
@@ -100,9 +36,9 @@ const ProjectsList = () => {
     };
 
     const [contentVisible, setContentVisible] = useState(false);
-  
+
     useEffect(() => {
-      setContentVisible(true);
+        setContentVisible(true);
     }, []);
 
     const leftColumnProjects = projects.filter((_, index) => index % 2 === 0);
@@ -110,19 +46,18 @@ const ProjectsList = () => {
 
     return (
         <div className="projects-cards-container">
-
             <div className={`cards-col-1 ${contentVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10'}`}>
                 {leftColumnProjects.map((project, index) => (
                     <motion.div
                         key={index}
-                        layoutId={project.id}             
+                        layoutId={project.id}
                         className='project-card'
                         style={{ height: (windowWidth < 640 || (windowWidth >= 1024 && windowWidth < 1400)) ? '450px' : '600px', width: (windowWidth < 640 || (windowWidth >= 1024 && windowWidth < 1400)) ? '300px' : '480px' }}
-                        onClick={() => openProjectDetails(project, 2 * index)}
+                        onClick={() => openProjectDetails(project)}
                     >
                         <div className="card-content">
                             <div className="card-image-container">
-                                <img src={projectImages[2 * index]} alt={project.title} className="project-image" />
+                                <img src={project.image} alt={project.alt} className="project-image" />
                             </div>
                             <div className="card-text-container">
                                 <h2 className="text-xl text-center font-bold md:mb-2 mb-1">{project.title}</h2>
@@ -140,11 +75,11 @@ const ProjectsList = () => {
                         layoutId={project.id}
                         className={`project-card ${contentVisible ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10'}`}
                         style={{ height: (windowWidth < 640 || (windowWidth >= 1024 && windowWidth < 1400)) ? '450px' : '600px', width: (windowWidth < 640 || (windowWidth >= 1024 && windowWidth < 1400)) ? '300px' : '480px' }}
-                        onClick={() => openProjectDetails(project, 2 * index + 1)}
+                        onClick={() => openProjectDetails(project)}
                     >
                         <div className="card-content">
                             <div className="card-image-container">
-                                <img src={projectImages[2 * index + 1]} alt={project.title} className="project-image" />
+                                <img src={project.image} alt={project.image} className="project-image" />
                             </div>
                             <div className="card-text-container">
                                 <h2 className="text-xl text-center font-bold md:mb-2 mb-1">{project.title}</h2>
@@ -157,16 +92,16 @@ const ProjectsList = () => {
 
             <AnimatePresence>
                 {selectedProject && (
-                    <motion.div 
-                        layoutId={selectedProject.id} 
-                        className="selected-project-screen"  // Increased padding to 10
+                    <motion.div
+                        layoutId={selectedProject.id}
+                        className="selected-project-screen"
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.3 }}
                         onClick={handleBackdropClick}
                     >
-                        <motion.div 
+                        <motion.div
                             className="selected-project-container"
                             initial="hidden"
                             animate="visible"
@@ -175,11 +110,10 @@ const ProjectsList = () => {
                                 hidden: { opacity: 0 },
                                 visible: { opacity: 1, transition: { staggerChildren: 0.2 } }
                             }}
-                            style={{ maxHeight: '90vh' }} // Ensure the modal doesn't exceed the viewport height
+                            style={{ maxHeight: '90vh' }}
                         >
-
                             <div className="selected-project-image-container">
-                                <motion.img src={projectImages[imageIdx]} alt={selectedProject.title} className="selected-project-image" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }}/>
+                                <motion.img src={selectedProject.image} alt={selectedProject.alt} className="selected-project-image" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.2 }} />
                             </div>
                             <div className="selected-project-info-container">
                                 <motion.h2 variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }} className="text-2xl font-bold mb-4">{selectedProject.title}</motion.h2>
@@ -190,7 +124,7 @@ const ProjectsList = () => {
                                         {selectedProject.details.techStack.map((tech, index) => (
                                             <li key={index} className="tech-stack-item">
                                                 <a href={tech.link} target="_blank" rel="noopener noreferrer" className="tech-stack-link">
-                                                    <img src={techIcons[tech.name]} alt={tech.name} className="tech-stack-icon" />
+                                                    <img src={tech.img} alt={tech.name} className="tech-stack-icon" />
                                                     <span className="proj-tooltip">{tech.name}</span>
                                                 </a>
                                             </li>
@@ -199,8 +133,8 @@ const ProjectsList = () => {
                                 </motion.div>
                                 <motion.div variants={{ hidden: { opacity: 0, y: 20 }, visible: { opacity: 1, y: 0 } }} className="selected-project-buttons-container">
                                     <div className="flex md:space-x-4 space-x-1 justify-center">
-                                        <a href={selectedProject.details.github} onClick={(e) => {if (gitPrivate[imageIdx]) {e.preventDefault(); alert('For Privacy Reasons, this Repository cannot be Public. To learn more about this project, Contact Me!'); }}} target="_blank" rel="noopener noreferrer" className="button-github">GitHub Repository</a>
-                                        <a href={selectedProject.details.liveDemo} onClick={(e) => {if (demoSoon[imageIdx]) {e.preventDefault(); alert('Demo Coming Soon!'); }}} target="_blank" rel="noopener noreferrer" className="button-demo">Live Demo</a>
+                                        <a href={selectedProject.details.github} onClick={(e) => { if (selectedProject.details.github === 'priv-repo') { e.preventDefault(); alert('For Privacy Reasons, this Repository cannot be Public. To learn more about this project, Contact Me!'); } }} target="_blank" rel="noopener noreferrer" className="button-github">GitHub Repository</a>
+                                        <a href={selectedProject.details.liveDemo} onClick={(e) => { if (selectedProject.details.liveDemo === 'no-demo') { e.preventDefault(); alert('Demo Coming Soon!'); } }} target="_blank" rel="noopener noreferrer" className="button-demo">Live Demo</a>
                                     </div>
                                     <motion.button onClick={closeProjectDetails} className="button-close">Close</motion.button>
                                 </motion.div>
@@ -210,7 +144,6 @@ const ProjectsList = () => {
                 )}
             </AnimatePresence>
         </div>
-
     );
 };
 

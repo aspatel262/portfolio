74d19e7b7360e8b02dbdec5ed3f3c6905ff3d425
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-07-08 02:06:24 -0400

    data struct in place created, plus new package instal

---
+++ src/assets/experience-data/RightNowData.js
@@ -0,0 +1,19 @@
+// rightNowData.js
+import image1 from '../aboutme/headshot.png';
+import image2 from '../aboutme/headshot.png';
+import image3 from '../aboutme/headshot.png';
+
+export const rightNowData = [
+  {
+    image: image1,
+    caption: 'Exploring the mountains and enjoying the breathtaking views.'
+  },
+  {
+    image: image2,
+    caption: 'Working on a new exciting project in the tech space.'
+  },
+  {
+    image: image3,
+    caption: 'Spending quality time with family and friends.'
+  }
+];

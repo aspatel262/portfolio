308e772e3732c809e389bcd1b8a68ab554c87420
aspatel262 <110962081+aspatel262@users.noreply.github.com>
2024-07-09 02:24:39 -0400

    photogrid component complete - about me finished

--- src/assets/photo-data/photoData.js
+++ src/assets/photo-data/photoData.js
@@ -1,4 +1,11 @@
-// imageData.js
+import bolly from "./bolly.JPEG";
+import car from "./car.jpg";
+import charlton from "./charlton.JPEG";
+import code from "./code.JPEG";
+import downtown from "./downtown.jpg";
+import dtw from "./dtw.JPEG";
+import giraffe from "./giraffe.JPEG";
+import glaive from "./glaive.JPEG";
 import image1 from "./image1.jpg";
 import image2 from "./image2.jpg";
 import image3 from "./image3.jpg";
@@ -7,8 +14,21 @@ import image5 from "./image5.jpg";
 import image6 from "./image6.jpg";
 import image7 from "./image7.jpg";
 import image8 from "./image8.jpg";
+import minigolf from "./minigolf.JPEG";
+import pingpong from "./pingpong.JPEG";
+import poker from "./poker.jpg";
+import rainwow from "./rainwow.JPEG";
+import tigers from "./tigers.JPEG";
 
 const images = [
+  { src: bolly, alt: "Bolly Image", paddingTop: "75%" },
+  { src: car, alt: "Car Image", paddingTop: "75%" },
+  { src: charlton, alt: "Charlton Image", paddingTop: "75%" },
+  { src: code, alt: "Code Image", paddingTop: "75%" },
+  { src: downtown, alt: "Downtown Image", paddingTop: "75%" },
+  { src: dtw, alt: "DTW Image", paddingTop: "75%" },
+  { src: giraffe, alt: "Giraffe Image", paddingTop: "75%" },
+  { src: glaive, alt: "Glaive Image", paddingTop: "75%" },
   { src: image1, alt: "Sample Image 1", paddingTop: "75%" },
   { src: image2, alt: "Sample Image 2", paddingTop: "66.67%" },
   { src: image3, alt: "Sample Image 3", paddingTop: "75%" },
@@ -16,7 +36,12 @@ const images = [
   { src: image5, alt: "Sample Image 5", paddingTop: "140%" },
   { src: image6, alt: "Sample Image 6", paddingTop: "71.43%" },
   { src: image7, alt: "Sample Image 7", paddingTop: "133.33%" },
-  { src: image8, alt: "Sample Image 8", paddingTop: "100%" }
+  { src: image8, alt: "Sample Image 8", paddingTop: "100%" },
+  { src: minigolf, alt: "Minigolf Image", paddingTop: "75%" },
+  { src: pingpong, alt: "Pingpong Image", paddingTop: "75%" },
+  { src: poker, alt: "Poker Image", paddingTop: "75%" },
+  { src: rainwow, alt: "Rainwow Image", paddingTop: "75%" },
+  { src: tigers, alt: "Tigers Image", paddingTop: "75%" }
 ];
 
 export default images;
--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -1,52 +1,87 @@
 import React from 'react';
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
-import image1 from '../assets/photo-data/image1.jpg';
-import image2 from '../assets/photo-data/image2.jpg';
-import image3 from '../assets/photo-data/image3.jpg';
-import image4 from '../assets/photo-data/image4.jpg';
-import image5 from '../assets/photo-data/image5.jpg';
-import image6 from '../assets/photo-data/image6.jpg';
-import image7 from '../assets/photo-data/image7.jpg';
-import image8 from '../assets/photo-data/image8.jpg';
+
+import bolly from '../assets/photo-data/bolly.JPEG';
+import car from '../assets/photo-data/car.jpg';
+import beach from '../assets/photo-data/beach.JPEG'
+import dtw from '../assets/photo-data/dtw.JPEG';
+import giraffe from '../assets/photo-data/giraffe.JPEG';
+import glaive from '../assets/photo-data/glaive.JPEG';
+import minigolf from '../assets/photo-data/minigolf.JPEG';
+import pingpong from '../assets/photo-data/pingpong.JPEG';
+import poker from '../assets/photo-data/poker.jpg';
+import rainwow from '../assets/photo-data/rainwow.JPEG';
+import tigers from '../assets/photo-data/tigers.JPEG';
+
+
+
+import AnimatedTyping from './AnimatedTyping.jsx';
 
 const PhotoCollage = () => {
   return (
     <ParallaxProvider>
       <div className="bg-gradient-to-r from-black via-gray-800 to-gray-800 py-16">
+        <div className="text-center mb-8">
+          <AnimatedTyping
+            textArray={['My Life As Photos Lately  ', 'Come See What I\'ve Seen  ', 'Look Through My Lens  ']}
+            fontSize="text-5xl break-all whitespace-normal"
+            color="text-white"
+            fontWeight="font-extrabold"
+            padding="pt-10"
+            subtextGradient="linear-gradient(to right, #4facfe 0%, #87cefa 100%)"
+            subtextSize="text-xl"
+            subtextPadding="mt-2"
+          />
+        </div>
         <div className="flex flex-wrap justify-center items-center">
           <Parallax className="flex flex-col" y={[-20, 20]}>
             <div className="p-2 bg-white">
-              <img src={image1} alt="1" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={tigers} alt="1" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
             <div className="p-2 bg-white">
-              <img src={image2} alt="2" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={poker} alt="2" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
             <div className="p-2 bg-white">
-              <img src={image3} alt="3" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={rainwow} alt="3" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+            </div>
+            <div className="p-2 bg-white">
+              <img src={bolly} alt="10" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
           </Parallax>
           <Parallax className="flex flex-col" y={[-15, 15]}>
             <div className="p-2 bg-white">
-              <img src={image4} alt="4" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={pingpong} alt="4" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
             <div className="p-2 bg-white">
-              <img src={image5} alt="5" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={giraffe} alt="5" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+            </div>
+            <div className="p-2 bg-white">
+              <img src={glaive} alt="9" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
           </Parallax>
           <Parallax className="flex flex-col" y={[-10, 10]}>
             <div className="p-2 bg-white">
-              <img src={image6} alt="6" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={car} alt="6" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
             <div className="p-2 bg-white">
-              <img src={image7} alt="7" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={minigolf} alt="7" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
             <div className="p-2 bg-white">
-              <img src={image8} alt="8" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+              <img src={dtw} alt="8" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
+            </div>
+            <div className="p-2 bg-white">
+              <img src={beach} alt="12" className="w-full sm:w-48 md:w-64 lg:w-80 xl:w-96 object-cover" />
             </div>
           </Parallax>
         </div>
       </div>
+      <div class="bg-gradient-to-r from-black via-gray-800 to-gray-800 py-16 text-white font-extrabold pt-10 text-3xl break-all whitespace-normal text-center">
+        <a href="https://sites.google.com/umich.edu/aditya-patel-photography/home" target="_blank" rel="noreferrer" class="text-white underline hover:text-gray-400">
+            Click to see more of my photography passion!
+        </a>
+      </div>
     </ParallaxProvider>
+    
   );
 };
 
